e5a25ccec70abd7a0d5efab25b4a6a99ff3c589e
github user name <ryunosuke_yamamoto@yahoo.co.jp>
2019-11-22 12:01:03 +0900

    自動更新機能の実装完了
    What
    新たに自動更新に関する機能をメッセージにて実施
    why
    画面分割検証にて片方がメッセージを送るだけで、もう片方にも7秒後に反映が行われるのを実装したいため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function () {
   function buildHTML(message){
     image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
-    let html = `<div class="messages_message">
+    let html = `<div class="messages_message" data-message-id="${message.id}">
                     <div class="messages_message__upper-info">
                       <div class="messages_message__upper-info__talker">
                         ${message.user_name}
@@ -17,30 +17,57 @@ $(document).on('turbolinks:load', function () {
                         ${image} 
                     </div>
                   </div>`  
-    return html;           
+      return html;         
   }  
               
-$('.new_message').on('submit', function(e){   
-      e.preventDefault(); 
-      let formData = new FormData(this); 
-      let url = $(this).attr('action')
-      $.ajax({ 
-        url: url, 
-        type: "POST", 
-        data: formData, 
-        dataType: 'json', 
-        processData: false, 
-        contentType: false 
-      }) 
-    .done(function(data){
-        html = buildHTML(data);
-        $('.messages').append(html);  
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
-        $('form')[0].reset();
-    })
-    .fail(function(){
-      alert("メッセージ送信に失敗しました");
-    });
-    return false;      
+  $('.new_message').on('submit', function(e){   
+        e.preventDefault(); 
+        let formData = new FormData(this); 
+        let url = $(this).attr('action')
+        $.ajax({ 
+          url: url, 
+          type: "POST", 
+          data: formData, 
+          dataType: 'json', 
+          processData: false, 
+          contentType: false 
+        }) 
+      .done(function(data){
+          html = buildHTML(data);
+          $('.messages').append(html);  
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+          $('form')[0].reset();
+      })
+      .fail(function(){
+        alert("メッセージ送信に失敗しました");
+      });
+      return false;      
   });
-})
+  
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.messages_message:last').data("message-id");
+
+      $.ajax({
+        url: "api/messages",
+        type: 'get', 
+        dataType: 'json',
+        data: {last_id: last_message_id}
+      })
+      .done(function (messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+
+        })
+
+      })
+      .fail(function () {
+        alert('自動更新に失敗しました');
+      });
+    } 
+  };
+  setInterval(reloadMessages, 7000);
+});   
