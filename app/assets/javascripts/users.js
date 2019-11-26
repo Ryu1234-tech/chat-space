aaf46308a84715eaabcd843d7ff8b4600e3126d2
github user name <ryunosuke_yamamoto@yahoo.co.jp>
2019-11-26 20:18:41 +0900

    jsの編集

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load',function() {
+$(function()  {
   function addUser(user) {
     let html = `
       <div class="chat-group-user clearfix">
@@ -55,7 +55,7 @@ $(document).on('turbolinks:load',function() {
       });
   });
   $(document).on("click", ".chat-group-user__btn--add", function() {
-    console.log
+    console.log("hello")
     const userName = $(this).attr("data-user-name");
     const userId = $(this).attr("data-user-id");
     $(this)
@@ -70,8 +70,6 @@ $(document).on('turbolinks:load',function() {
       .remove();
   });
 });
-
-
 
 
 
